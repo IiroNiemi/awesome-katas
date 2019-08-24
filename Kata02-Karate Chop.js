64cbf545d1d3c323cdbff3aa62c081a02fb5f582
Iiro <niemi.iiro@gmail.com>
2019-08-24 16:09:02 +0300

    Slicing and rounding

--- Kata02-Karate Chop.js
+++ Kata02-Karate Chop.js
@@ -1,3 +1,17 @@
-let array_of_int = [1, 2, 3, 4, 5]
+let array_of_ints = [11, 22, 33, 44, 55, 66, 77, 88, 99]
 
-console.log(array_of_int);
+console.log(chop(array_of_ints));
+
+function chop(arrayOfInts){
+	var cutPoint = Math.round(arrayOfInts.length / 2);
+	var firstSlice = arrayOfInts.slice(0, Math.round(arrayOfInts.length / 2));
+	var secondSlice = arrayOfInts.slice(cutPoint, arrayOfInts.length);
+
+	console.log(firstSlice);
+	console.log(secondSlice);
+
+	var retval = arrayOfInts.map(Math.sqrt);
+
+	return retval;
+}
+
