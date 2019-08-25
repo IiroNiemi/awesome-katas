27374caa5f57230aac53ad4da5eea1e12d26b135
IiroNiemi <niemi.iiro@gmail.com>
2019-08-25 11:05:13 +0300

    new implementation

--- Kata02-Karate Chop.js
+++ Kata02-Karate Chop.js
@@ -1,33 +1,23 @@
 let array_of_ints = [11, 22, 33, 44, 55, 66, 77, 88, 99]
 
-//console.log(chop(array_of_ints));
+//chop(0, array_of_ints);
 
-chop(0, array_of_ints);
+
 
 
 function chop(find, arrayOfInts){
 
+	let index = BinarySearch(find, arrayOfInts, 0 , arrayOfInts.length - 1)	
 	
-	while(arrayOfInts.length > 2){
-		var containingSlice = ReturnContainingHalf(find, arrayOfInts)	
-		console.log(containingSlice);
-	}
+	
 	
 
 	//return retval;
 }
 
-var ReturnContainingHalf = function(find, arrayOfInts){
-	if(arrayOfInts.includes(find)){
-		return arrayOfInts;
-	} else {
-		let cutPoint = Math.round(arrayOfInts.length / 2);
-		let firstSlice = arrayOfInts.slice(0, Math.round(arrayOfInts.length / 2));
-		let secondSlice = arrayOfInts.slice(cutPoint, arrayOfInts.length);
+var BinarySearch = function(find, arrayOfInts, left, right) {
 	
-		return firstSlice.includes(find) ? ReturnContainingHalf(find, firstSlice) : 
-																			ReturnContainingHalf(find, secondSlice); 
-	}
 	
 }
 
+//https://www.geeksforgeeks.org/binary-search/
