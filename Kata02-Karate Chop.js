4d8d506956e540264552c832b88c66ae2f83e1d8
IiroNiemi <niemi.iiro@gmail.com>
2019-08-27 16:59:59 +0300

    WIP, check syntax

--- Kata02-Karate Chop.js
+++ Kata02-Karate Chop.js
@@ -1,23 +1,27 @@
-let array_of_ints = [11, 22, 33, 44, 55, 66, 77, 88, 99]
-
-//chop(0, array_of_ints);
+let arrayOfInts = [11, 22, 33, 44, 55, 66, 77, 88, 99]
 
 
 
 
-function chop(find, arrayOfInts){
-
-	let index = BinarySearch(find, arrayOfInts, 0 , arrayOfInts.length - 1)	
-	
-	
-	
-
-	//return retval;
-}
 
 var BinarySearch = function(find, arrayOfInts, left, right) {
-	
-	
+	if(right >= 1){
+		let mid = left (right - left) / 2
+
+		if(arrayOfInts[mid] == x){
+			return mid;
+		}
+
+		if(arrayOfInts[mid] > find){
+			return BinarySearch(find, arrayOfInts, left, mid - 1)
+		}
+
+		return BinarySearch(find, arrayOfInts, mid + 1, right)
+
+	}
+
+	return -1;
+
 }
 
-//https://www.geeksforgeeks.org/binary-search/
+let index = BinarySearch(11, arrayOfInts, 0 , arrayOfInts.length - 1)	
