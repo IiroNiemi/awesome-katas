4aef93d3df04677490a09b3e9f3507ea04a139e0
Iiro <niemi.iiro@gmail.com>
2019-09-03 19:56:26 +0300

    gitignore added

--- Kata02-Karate Chop.js
+++ Kata02-Karate Chop.js
@@ -1,4 +1,4 @@
-let arrayOfInts = [11, 22, 33, 44, 55, 66, 77, 88, 99]
+let arrayOfInts = [11, 22, 33, 44, 55, 66, 77, 88]
 
 
 
@@ -6,17 +6,23 @@ let arrayOfInts = [11, 22, 33, 44, 55, 66, 77, 88, 99]
 
 var BinarySearch = function(find, arrayOfInts, left, right) {
 	if(right >= 1){
-		let eval = right - left;
-		let mid = eval / 2;
+		
+
+		//How this should be rounded properly?
+		let mid = Math.round(left + (right - left) / 2);
+
 
 		if(arrayOfInts[mid] == find){
 			return mid;
 		}
+		
+		console.log(mid);
+		console.log(arrayOfInts[mid])
 
 		if(arrayOfInts[mid] > find){
 			return BinarySearch(find, arrayOfInts, left, mid - 1)
 		}
-
+		
 		return BinarySearch(find, arrayOfInts, mid + 1, right)
 
 	}
@@ -26,3 +32,5 @@ var BinarySearch = function(find, arrayOfInts, left, right) {
 }
 
 let index = BinarySearch(11, arrayOfInts, 0 , arrayOfInts.length - 1)	
+
+console.log("Result: " + index);
