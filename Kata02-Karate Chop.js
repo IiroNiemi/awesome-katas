68aacb7950af7d55fbc0f563eee00f93f7e84d4d
Iiro <niemi.iiro@gmail.com>
2019-09-03 20:17:35 +0300

    Solved Karate chop

--- Kata02-Karate Chop.js
+++ Kata02-Karate Chop.js
@@ -1,36 +1,24 @@
 let arrayOfInts = [11, 22, 33, 44, 55, 66, 77, 88]
 
 
-
-
-
 var BinarySearch = function(find, arrayOfInts, left, right) {
-	if(right >= 1){
+	
+	if (left > right) return -1;
 		
+		let mid = Math.floor((right + left) / 2);
 
-		//How this should be rounded properly?
-		let mid = Math.round(left + (right - left) / 2);
-
-
-		if(arrayOfInts[mid] == find){
+		if(arrayOfInts[mid] === find){
 			return mid;
 		}
-		
-		console.log(mid);
-		console.log(arrayOfInts[mid])
 
 		if(arrayOfInts[mid] > find){
 			return BinarySearch(find, arrayOfInts, left, mid - 1)
 		}
 		
-		return BinarySearch(find, arrayOfInts, mid + 1, right)
-
-	}
-
-	return -1;
+		return BinarySearch(find, arrayOfInts, mid + 1, right)	
 
 }
 
-let index = BinarySearch(11, arrayOfInts, 0 , arrayOfInts.length - 1)	
+let index = BinarySearch(88, arrayOfInts, 0 , arrayOfInts.length - 1)	
 
 console.log("Result: " + index);
