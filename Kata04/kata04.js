8517e1e34c744703f4c2915fd99827d8a5c428a0
Iiro <niemi.iiro@gmail.com>
2019-11-29 19:35:44 +0200

    File reading init

--- Kata04/kata04.js
+++ Kata04/kata04.js
@@ -1,10 +1,21 @@
 let fs = require("fs");
 
-let strg = fs.readFileSync("./weather.dat").toString().split("\n");
+let dirtyRows = fs.readFileSync("./weather.dat").toString().split("\n");
+let TotalRowNumbers = dirtyRows.length;
+let rows = dirtyRows.slice(2, TotalRowNumbers);
+let lineWords = rows[0].split("  ");
+let data = lineWords. // TODO; Clear white spaces from the lineWords
 
-console.log(strg.length);
+console.log(data);
 
-//console.log(strg[2].split("  "));
+
+
+
+
+// let thrdRow = strg[2].split("  ");
+// console.log("Highest: " + thrdRow[2] +" Lowest: " + thrdRow[4] )
+
+
 
 
 
