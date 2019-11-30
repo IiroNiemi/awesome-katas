cccf6194543592b08ea2d9f809ad6a1b1bb278af
IiroNiemi <niemi.iiro@gmail.com>
2019-11-30 15:04:10 +0200

    Kata04: First part ready

--- Kata04/kata04.js
+++ Kata04/kata04.js
@@ -2,22 +2,40 @@ let fs = require("fs");
 
 let dirtyRows = fs.readFileSync("./weather.dat").toString().split("\n");
 let TotalRowNumbers = dirtyRows.length;
-let rows = dirtyRows.slice(2, TotalRowNumbers);
+let rows = dirtyRows.slice(2, TotalRowNumbers - 2);
 
 let lineWords = rows.map(rows=>{
   let arrayOfWords = rows.split("  ");
 
   let cleanWords = arrayOfWords.filter(word => {
-    return word != ''
+
+    let clnWord = ""; 
+    if(word != ''){
+      clnWord = word;
+    } 
+    return clnWord
   });
   
   return cleanWords
 })
 
 
+let daySpread = {
+  day: 0,
+  spread: 100,
+};
 
+lineWords.forEach(word => {
+  let day = word[0]
+  let maxT = word[1].replace('*', '')
+  let minT = word[2].replace('*', '')
+  let spread = maxT - minT; 
+  if(spread < daySpread.spread) {
+    daySpread.spread = spread;
+    daySpread.day = day;
+  }
+  // console.log("day: " + day + " Spread: " + daySpread)
 
-console.log(lineWords);
+});
 
-
-
+console.log(daySpread);
