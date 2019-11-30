e4d0035b5d71a614252ec8c567893e8d03b32442
IiroNiemi <niemi.iiro@gmail.com>
2019-11-30 13:36:17 +0200

    Array of Rows contains array of words

--- Kata04/kata04.js
+++ Kata04/kata04.js
@@ -3,21 +3,21 @@ let fs = require("fs");
 let dirtyRows = fs.readFileSync("./weather.dat").toString().split("\n");
 let TotalRowNumbers = dirtyRows.length;
 let rows = dirtyRows.slice(2, TotalRowNumbers);
-let lineWords = rows[0].split("  ");
-let data = lineWords. // TODO; Clear white spaces from the lineWords
 
-console.log(data);
+let lineWords = rows.map(rows=>{
+  let arrayOfWords = rows.split("  ");
 
-
-
-
-
-// let thrdRow = strg[2].split("  ");
-// console.log("Highest: " + thrdRow[2] +" Lowest: " + thrdRow[4] )
-
+  let cleanWords = arrayOfWords.filter(word => {
+    return word != ''
+  });
+  
+  return cleanWords
+})
 
 
 
 
-// console.log
+console.log(lineWords);
+
+
 
