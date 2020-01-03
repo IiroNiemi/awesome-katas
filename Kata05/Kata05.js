e13939307034353b9654a4d499d18f9fed5d12c8
Iiro Niemi <iiro.niemi@profitsoftware.com>
2020-01-03 15:29:24 +0200

    Buffer implementation

--- Kata05/Kata05.js
+++ Kata05/Kata05.js
@@ -1,12 +1,29 @@
 let fs = require("fs");
-let words = fs.readFileSync("./american-english-copy.txt").toString().split("\n")
+let allWords = fs.readFileSync("./american-english-copy.txt").toString().split("\n")
+const wordArray = new Uint16Array(10);
 
 var crypto = require('crypto');
-var hash = crypto.createHash("md5").update("value").digest("hex");
-
-const bloomFilter = new Uint16Array(10);
-bloomFilter[0] = 1;
+let hash = crypto.createHash("md5").update("A").digest("hex");
 
 
+// https://nodejs.org/dist/latest-v6.x/docs/api/buffer.html#buffer_buf_tostring_encoding_start_end
+const buf1 = Buffer.allocUnsafe()
+let A = "A";
 
-console.log(hash)
+console.log(A.toString("ascii"))
+
+/*
+for (let i = 0; i < allWords.length; i++) {
+    const word = allWords[i];
+    wordArray[i] = crypto.createHash("md5").update(word).digest("hex");
+    
+}
+*/
+
+
+
+
+
+
+
+
