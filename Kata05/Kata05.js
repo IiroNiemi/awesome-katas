25a50f6cceaf76a415ba654c3984be4d9c785ffd
Iiro Niemi <iiro.niemi@profitsoftware.com>
2020-01-03 13:52:18 +0200

    File read and md5 hash in use

--- Kata05/Kata05.js
+++ Kata05/Kata05.js
@@ -1,3 +1,12 @@
 let fs = require("fs");
+let words = fs.readFileSync("./american-english-copy.txt").toString().split("\n")
 
-console.log(fs.readFileSync("./american-english-copy.txt").toString().split("\n"));
+var crypto = require('crypto');
+var hash = crypto.createHash("md5").update("value").digest("hex");
+
+const bloomFilter = new Uint16Array(10);
+bloomFilter[0] = 1;
+
+
+
+console.log(hash)
