4e5c44a93a6914f330e2a330275fc4251c07f4be
Iiro <niemi.iiro@gmail.com>
2020-01-04 16:03:05 +0200

    Formatting

--- Kata05/Kata05.js
+++ Kata05/Kata05.js
@@ -2,20 +2,22 @@ let fs = require("fs");
 var crypto = require('crypto');
 let allWords = fs.readFileSync("./american-english-copy.txt").toString().split("\n")
 
-const unSafeBuf = Buffer.allocUnsafe(allWords.length)
+
 const allocBuf = Buffer.alloc(64, "0")
-const fromBuf = Buffer.from("Hello world")
 
 let pointer = 0;
 
 allWords.forEach(word => {
   let hash = crypto.createHash("md5").update(word).digest("hex");
-  console.log(hash)
+
+  console.log(word + " = " +hash)
+  console.log(allocBuf.toString() + "\n")
+  
   allocBuf.write(hash, pointer)
   pointer += hash.length;
 });
 
-
+console.log("Final output: ")
 console.log(allocBuf.toString())
 
 
