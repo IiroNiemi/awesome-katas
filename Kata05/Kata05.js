64cbd141160ec3d851db6c67af4ee8b6bea09ad0
Iiro <niemi.iiro@gmail.com>
2020-01-05 14:03:34 +0200

    reading input

--- Kata05/Kata05.js
+++ Kata05/Kata05.js
@@ -1,17 +1,23 @@
 let fs = require("fs");
-var crypto = require('crypto');
+let crypto = require('crypto');
+
+let readLine = require('readline')
+var rl = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 let allWords = fs.readFileSync("./american-english-copy.txt").toString().split("\n")
 
+// const allocBuf = Buffer.allocUnsafe(allWords.length * 32) // md5 hash is 32 digit long, lets allocate memory for those bytes.
+const allocBuf = Buffer.alloc(allWords.length * 32, "0") // Manually initialize memory. (safe but slower)
 
-const allocBuf = Buffer.alloc(64, "0")
-
-let pointer = 0;
-
+let pointer = 0; // Points where next hash will be written.
 allWords.forEach(word => {
   let hash = crypto.createHash("md5").update(word).digest("hex");
 
-  console.log(word + " = " +hash)
-  console.log(allocBuf.toString() + "\n")
+// console.log(word + " = " +hash)
+// console.log(allocBuf.toString() + "\n")
   
   allocBuf.write(hash, pointer)
   pointer += hash.length;
@@ -20,10 +26,26 @@ allWords.forEach(word => {
 console.log("Final output: ")
 console.log(allocBuf.toString())
 
+rl.question("Spell check: ", function(answer) {
+  compareHash(answer);
+  console.log("answer:", answer);
+
+  rl.close();
+});
 
 
+function compareHash(answer){
+  let answerHash = crypto.createHash("md5").update(answer).digest("hex");
+  console.log("Answer hash: " + answerHash);
+  // Keksi miten muodostettu hash-jono puretaan muistista fiksusti
+  allocBuf.forEach(hash => {
+    if(hash === answerHash){
+      console.log("FOUND!");
+    }
+  
+  });
 
-
+}
 
 
 
