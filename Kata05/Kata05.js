61bdd6077320614cecf89b38ebc3517bc61ca22a
Iiro <niemi.iiro@gmail.com>
2020-01-06 09:39:25 +0200

    Code formatting and cleanup

--- Kata05/Kata05.js
+++ Kata05/Kata05.js
@@ -1,49 +1,42 @@
 let fs = require("fs");
 let crypto = require('crypto');
-
+let allWords = fs.readFileSync("./american-english-copy.txt").toString().split("\n")
+// const allocBuf = Buffer.allocUnsafe(allWords.length * 32) // md5 hash is 32 digit long, lets allocate memory for those bytes.
+const allocBuf = Buffer.alloc(allWords.length * 32, "0") // Manually initialize memory. (safe but slower)
 let readLine = require('readline')
 var rl = readLine.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-let allWords = fs.readFileSync("./american-english-copy.txt").toString().split("\n")
-
-// const allocBuf = Buffer.allocUnsafe(allWords.length * 32) // md5 hash is 32 digit long, lets allocate memory for those bytes.
-const allocBuf = Buffer.alloc(allWords.length * 32, "0") // Manually initialize memory. (safe but slower)
-
 let pointer = 0; // Points where next hash will be written.
 allWords.forEach(word => {
-  let hash = crypto.createHash("md5").update(word).digest("hex");
+	let hash = crypto.createHash("md5").update(word).digest("hex");
 
 // console.log(word + " = " +hash)
 // console.log(allocBuf.toString() + "\n")
-  
-  allocBuf.write(hash, pointer)
-  pointer += hash.length;
+
+	allocBuf.write(hash, pointer)
+	pointer += hash.length;
 });
 
 console.log("Final output: ")
 console.log(allocBuf.toString())
 
 rl.question("Spell check: ", function(answer) {
-  compareHash(answer);
-  console.log("answer:", answer);
+	compareHash(answer);
+	console.log("answer:", answer);
 
-  rl.close();
+	rl.close();
 });
 
 
 function compareHash(answer){
-  let answerHash = crypto.createHash("md5").update(answer).digest("hex");
-  console.log("Answer hash: " + answerHash);
-  // Keksi miten muodostettu hash-jono puretaan muistista fiksusti
-  allocBuf.forEach(hash => {
-    if(hash === answerHash){
-      console.log("FOUND!");
-    }
-  
-  });
+	let answerHash = crypto.createHash("md5").update(answer).digest("hex");
+	console.log("Answer hash: " + answerHash);
+
+
+
 
 }
 
